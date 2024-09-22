ba513248691e9d2b0980ae438c556775a0806a2a
NguyenTanTai <ngtantai48@gmail.com>
2024-09-22 09:06:16 +0700

    + delete and edit channel

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -7,7 +7,9 @@ import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 import { useEffect, useState } from "react";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 
 export const Modalprovider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -29,6 +31,8 @@ export const Modalprovider = () => {
             <CreateChannelModal />
             <LeaveServerModal />
             <DeleteServerModal />
+            <DeleteChannelModal />
+            <EditChannelModal />
         </>
     )
 }
