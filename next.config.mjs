93c62a858d85dec9ed30a5fb3b42ec2b43ea7b0f
NguyenTanTai <ngtantai48@gmail.com>
2024-09-23 11:36:47 +0700

    + Chat input component

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "uploadthing.com",
-            "utfs.io"
-        ]
-    }
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'uploadthing.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
