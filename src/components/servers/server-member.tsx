a4ac458fa4bc16a3c633d22571163ed7695e2d3c
NguyenTanTai <ngtantai48@gmail.com>
2024-09-25 01:09:53 +0700

    + chore: comment

--- src/components/servers/server-member.tsx
+++ src/components/servers/server-member.tsx
@@ -17,18 +17,17 @@ const roleIconMap = {
     [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />
 }
 
-
 export const ServerMember = ({
     member,
-    server,
+    // server,
 }: ServerMemberProps) => {
     const params = useParams();
     const router = useRouter();
 
-    const icon = roleIconMap[member.role];
+    const icon = roleIconMap[member?.role];
 
     const onClick = () => {
-        router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
     }
 
     return (
@@ -36,20 +35,20 @@ export const ServerMember = ({
             onClick={onClick}
             className={cn(
                 "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
-                params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+                params?.memberId === member?.id && "bg-zinc-700/20 dark:bg-zinc-700"
             )}
         >
             <UserAvatar
-                src={member.profile.imageUrl}
+                src={member?.profile?.imageUrl}
                 className="h-8 w-8 md:h-8 md:w-8"
             />
             <p
                 className={cn(
                     "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-                    params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+                    params?.memberId === member?.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
                 )}
             >
-                {member.profile.name}
+                {member?.profile?.name}
             </p>
             {icon}
         </button>
